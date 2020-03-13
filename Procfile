76d3a6c4bf155e1630efd00f36c50f5765bf5d2f
akm0001 <anandmaurya@hotmail.com>
2020-03-13 19:05:04 +0100

    reset app

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn covidLiveTracker.wsgi --log-file -
+web: gunicorn gettingstarted.wsgi --log-file -
--- tracker/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from . import views
-urlpatterns = [
-    path('',views.index, name='index'),
-    path('data/',views.getData, name='data'),
-]
