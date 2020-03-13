ab9b3ae34f1c6565943f6d693fdb32f8799a0ca2
akm0001 <anandmaurya@hotmail.com>
2020-03-13 12:49:25 +0100

    modified settings

--- covidLiveTracker/settings.py
+++ covidLiveTracker/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR,'static'),
+]
 
 #CSRF_COOKIE_SECURE = True
 #SESSION_COOKIE_SECURE = True
