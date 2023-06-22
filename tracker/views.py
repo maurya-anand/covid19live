5093de519b6ec2e6c0e0f538859da66b539af8ce
maurya-anand <anandmaurya@hotmail.com>
2023-06-22 19:33:36 +0100

    updated banner cards

--- tracker/views.py
+++ tracker/views.py
@@ -40,20 +40,10 @@ def getData(request):
         'plotdata': [],
         'dt_table': []
     }
-    global_stats_res = make_request(url=global_stats_source_api)
-    if (global_stats_res):
-        if 'cases' in global_stats_res:
-            res_obj3['total_reported'] = global_stats_res['cases']
-        if 'recovered' in global_stats_res:
-            res_obj3['total_recovered'] = global_stats_res['recovered']
-        if 'deaths' in global_stats_res:
-            res_obj3['total_deaths'] = global_stats_res['deaths']
-        if 'active' in global_stats_res:
-            res_obj3['total_active'] = global_stats_res['active']
     res_arr2 = []
     dt_data = {'data': []}
-    per_country_stats_stats_res = make_request(
-        url=per_country_stats_source_api)
+    
+    per_country_stats_stats_res = make_request(url=per_country_stats_source_api)
     if (per_country_stats_stats_res):
         for country in per_country_stats_stats_res:
             if 'active' in country and 'countryInfo' in country and 'iso3' in country['countryInfo']:
@@ -66,19 +56,26 @@ def getData(request):
                     "deaths": country['deaths'],
                     "active": country['active']
                 })
-                dt_data['data'].append([country['country'],
-                                        country['countryInfo']['iso3'],
-                                        country['active'],
-                                        country['recovered'],
-                                        country['deaths'],
-                                        country['cases']]
-                                       )
-    res_obj3 = {
-        'total_reported': None,
-        'total_recovered': None,
-        'total_deaths': None,
-        'total_active': None,
-        'plotdata': json.dumps(res_arr2),
-        'dt_table': dt_data['data'],
-    }
+                dt_data['data'].append([
+                    country['country'],
+                    country['countryInfo']['iso3'],
+                    country['active'],
+                    country['recovered'],
+                    country['deaths'],
+                    country['cases']
+                    ])
+    global_stats_res = make_request(url=global_stats_source_api)
+    if (global_stats_res):
+        if 'cases' in global_stats_res:
+            res_obj3['total_reported'] = global_stats_res['cases']
+        if 'recovered' in global_stats_res:
+            res_obj3['total_recovered'] = global_stats_res['recovered']
+        if 'deaths' in global_stats_res:
+            res_obj3['total_deaths'] = global_stats_res['deaths']
+        if 'active' in global_stats_res:
+            res_obj3['total_active'] = global_stats_res['active']
+    
+    res_obj3['plotdata'] = json.dumps(res_arr2)
+    res_obj3['dt_table'] = dt_data['data']
+    
     return JsonResponse(res_obj3, safe=False)
